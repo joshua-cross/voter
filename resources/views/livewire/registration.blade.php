3e7089f4d6fb48c51e34cfd148e2522677cea781
joshua-cross <crossjoshua6@gmail.com>
2024-11-13 11:34:23 +0000

    Add correct fields to the registration page

--- resources/views/livewire/registration.blade.php
+++ resources/views/livewire/registration.blade.php
@@ -1,9 +1,9 @@
 <form class="mx-auto mt-20 max-w-lg px-2 sm:px-6 lg:px-8" method="post"
-      action="/forgot-password">
+      action="{{ route("register") }}">
     @csrf
     <h1 class="text-2xl mb-5 font-medium">Registration</h1>
     <div>
-        <label for="price" class="block text-sm/6 font-medium text-gray-900">Name</label>
+        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
         <div class="relative mt-1 rounded-md shadow-sm">
             <input
                 type="text"
@@ -18,7 +18,7 @@
         @enderror
     </div>
     <div>
-        <label for="price" class="block text-sm/6 font-medium text-gray-900">Email Address</label>
+        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email Address</label>
         <div class="relative mt-1 rounded-md shadow-sm">
             <input
                 type="email"
@@ -32,13 +32,41 @@
         <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
         @enderror
     </div>
-    @if(session("status") || $errors->has("email"))
-        <p class="text-xs mt-1">If an account exists with this email you should receive an email</p>
-    @endif
-    <div class="mt-2.5">
-        <button href="/forgot-password"
-                type="submit"
-                class="rounded-md bg-blue-600 px-3 py-2 text-sm font-medium text-white" aria-current="page">Submit
+    <div>
+        <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+        <div class="relative mt-1 rounded-md shadow-sm">
+            <input
+                type="password"
+                name="password"
+                id="password"
+                wire:model.live="form.password"
+                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+            >
+        </div>
+        @error('form.password')
+        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+        @enderror
+    </div>
+    <div>
+        <label for="password-confirmation" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
+        <div class="relative mt-1 rounded-md shadow-sm">
+            <input
+                type="password"
+                name="password_confirmation"
+                id="password-confirmation"
+                wire:model.live="form.password_confirmation"
+                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+            >
+        </div>
+        @error('form.password_confirmation')
+        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mt-2.5 flex items-center gap-5 justify-start">
+        <button
+            type="submit"
+            class="rounded-md bg-blue-600 px-3 py-2 text-sm font-medium text-white" aria-current="page">Register
         </button>
+        <a class="text-blue-400 text-sm hover:underline" href="{{ route('login') }}">Or Login</a>
     </div>
 </form>
