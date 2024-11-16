891397231d99510cf29dd8b75c2c80c6d5742fbd
joshua-cross <crossjoshua6@gmail.com>
2024-11-16 12:37:19 +0000

    Add user id to the responses and fix seeder to reflect

--- database/seeders/PollSeeder.php
+++ database/seeders/PollSeeder.php
@@ -5,16 +5,17 @@ namespace Database\Seeders;
 use App\Models\Option;
 use App\Models\Poll;
 use App\Models\Response;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class PollSeeder extends Seeder
 {
     public function run(): void
     {
-        Poll::factory(10)->expired()->create();
-        Poll::factory(10)->has(Option::factory()->has(Response::factory(rand(0, 20)))->count(rand(2, 10)),
+        Poll::factory(5)->expired()->create();
+        Poll::factory(5)->has(Option::factory()->has(Response::factory(rand(0, 20))->withUser(User::factory()->create()))->count(rand(2, 10)),
             'options')->private()->create();
-        Poll::factory(150)->has(Option::factory()->has(Response::factory(rand(0, 20)))->count(rand(2, 10)),
+        Poll::factory(20)->has(Option::factory()->has(Response::factory(rand(0, 20))->withUser(User::factory()->create()))->count(rand(2, 10)),
             'options')->notExpired()->create();
     }
 }
